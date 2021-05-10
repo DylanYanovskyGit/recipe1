41c9ba0870ab03f2be948291084b7337d408ee80
Dyl <dyl_yan@yahoo.com>
2021-05-09 22:29:01 -0700

    commit

---
+++ app/src/main/java/edu/sjsu/android/recipe1/ui/login/ingrediants.java
@@ -0,0 +1,6 @@
+package edu.sjsu.android.recipe1.ui.login;
+
+import android.app.Activity;
+
+public class ingrediants extends Activity {
+}
---
+++ app/src/main/java/edu/sjsu/android/recipe1/ui/login/ingredients.java
@@ -0,0 +1,16 @@
+package edu.sjsu.android.recipe1.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.sjsu.android.recipe1.R;
+
+public class ingredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingrediants);
+    }
+}
